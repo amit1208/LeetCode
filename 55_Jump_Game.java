530f3e302e1ef6445d090a5eaf5fa33f6df6703f
amit1208 <amitrajput.chauhan@gmail.com>
2025-02-07 17:41:02 +0530

    array

--- 55_Jump_Game.java
+++ 55_Jump_Game.java
@@ -1,8 +1,17 @@
 class Solution {
-    public boolean canJump(int[] nums) {
 
+    public boolean canJump(int[] nums) {
+        int l=nums.length-1;
+    for(int i=l-1;i>=0;i--){
+
+       if (i + nums[i] >= l) {
+                l = i;
+            }
+        }
+        return l == 0;
     }
 }
+
 
 
 
