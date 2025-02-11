e3c32d9e9144a4401b992a043d51f4134dcb64cc
amit1208 <amitrajput.chauhan@gmail.com>
2025-02-11 13:09:52 +0530

    string

--- 3174_clear_digits.java
+++ 3174_clear_digits.java
@@ -1,22 +1,56 @@
 class Solution {
-    public String clearDigits(String s) {
-        Stack<Character> stack = new Stack<>();
-        
-        for (char ch : s.toCharArray()) {
-            if (Character.isDigit(ch)) {
-                if (!stack.isEmpty()) {
-                    stack.pop();
-                }
-            } else {
-                stack.push(ch);
-            }
+    public String removeOccurrences(String s, String part) {
+        StringBuilder sb=new StringBuilder(s);
+
+        while(sb.indexOf(part)!=-1){
+            sb.delete(sb.indexOf(part),sb.indexOf(part)+part.length());
         }
-        
-        StringBuilder result = new StringBuilder();
-        for (char ch : stack) {
-            result.append(ch);
-        }
-        
-        return result.toString();
+
+        return sb.toString();            
+
     }
 }
+
+
+
+// class Solution {
+
+//     public String removeOccurrences(String s, String part) {
+//         if(s.equals(part)) return '' ;
+//         if(s.length()<part.length()) return s;
+
+//         Stack<Character> st=new Stack<>();
+//         int plen=part.length()-1;
+
+//          for(char c:s.toCharArray()){
+
+//             if(c == part.charAt(plen))
+//                 {
+
+//                     while(plen>0){
+//                         if(st.peek()==part.charAt(--plen)){
+//                             st.pop();
+//                     }
+//                     else {break;}
+
+//                     }
+//                     st.pop();
+//                 }
+//             else{
+//                 st.push(c);
+//             }
+
+//          }
+
+
+
+//         StringBuilder result = new StringBuilder();
+
+//         for(char ch:st) {
+//             result.append(ch);
+//         }
+        
+//         return result.toString();
+
+//     }
+// }
